c94fb62630710d9efb7e3895d282f1f6ff55089a
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-07-21 09:25:54 +0000

    Initial commit with introduction function

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def introduction():
+    print("Welcome to the Chronicles of Valor!")
+    print("Chronicles of Valor is a text based adventure game where YOU decide your destiny! You will venture forth into Cydoia, a flourishing continent with all kinds of adventures waiting for you.")
+    name = input("But first, go ahead and input your name: ")
+    return name
