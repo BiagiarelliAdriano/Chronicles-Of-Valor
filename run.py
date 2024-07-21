cd3fe123b135b1bb174b7ae2ccdda345c13f44b9
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-07-21 09:48:54 +0000

    Add main function

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 def introduction():
     print("Welcome to the Chronicles of Valor!")
-    print("Chronicles of Valor is a text based adventure game where YOU decide your destiny! You will venture forth into Cydoia, a flourishing continent with all kinds of adventures waiting for you.")
+    print("Chronicles of Valor is a text based adventure game where YOU decide your destiny! \nYou will venture forth into Cydoia, a flourishing continent with all kinds of adventures waiting for you.")
     name = input("But first, go ahead and input your name: ")
     return name
 
 def choose_class(name):
     print(f"Hello, {name}!")
-    print("In Chronicles of Valor you will be playing as one of the following classes: a Fighter, a Rogue or a Wizard. Each class has a unique story to tell and for you to shape, so go ahead and choose your favorite!")
+    print("In Chronicles of Valor you will be playing as one of the following classes: a Fighter, a Rogue or a Wizard. \nEach class has a unique story to tell and for you to shape, so go ahead and choose your favorite!")
     print("1. Fighter")
     print("2. Rogue")
     print("3. Wizard")
@@ -17,3 +17,9 @@ def choose_class(name):
         else:
             print("Invalid choice. Please choose 1, 2, or 3.")
 
+def main():
+    name = introduction()
+    player_class = choose_class(name)
+
+if __name__ == "__main__":
+    main()
